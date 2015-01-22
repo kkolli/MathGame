994355c3cbdada7fe77f7cefbe51082487f62224
Krishna Kolli <kkolli@ucsd.edu>
2015-01-21 21:16:26 -0800

    Initial Commit

---
+++ Speedy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speedy
+//
+//  Created by Krishna Kolli on 1/21/15.
+//  Copyright (c) 2015 Krishna Kolli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
