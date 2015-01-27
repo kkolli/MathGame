63ecbbe4fb79c6115a9b01d8ccc3bd195f68bf7f
Tyler Levine <tylevine@cisco.com>
2015-01-27 12:07:19 -0800

    spritekit is now working on the main game scene

--- client/Speedy/LoginViewController.swift
+++ client/Speedy/LoginViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SpriteKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
