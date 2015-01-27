657ab009a106149a27f9189829222283a1146d95
Tyler Levine <tylevine@cisco.com>
2015-01-27 12:07:19 -0800

    spritekit is now working on the main game scene

--- client/Speedy/LoginViewController.swift
+++ client/Speedy/LoginViewController.swift
@@ -1,65 +1,25 @@
 //
-// ViewController.swift
-// SwiftBook
+//  ViewController.swift
+//  Speedy
 //
-// Created by Brian Coleman on 2014-07-07.
-// Copyright (c) 2014 Brian Coleman. All rights reserved.
-//import UIKit
+//  Created by Krishna Kolli on 1/21/15.
+//  Copyright (c) 2015 Krishna Kolli. All rights reserved.
+//
 
 import UIKit
-import SpriteKit
 
-class ViewController: UIViewController, FBLoginViewDelegate {
-    
-    @IBOutlet var fbLoginView : FBLoginView!
-    
+class LoginViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view, typically from a nib.
-        
-        self.fbLoginView.delegate = self
-        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
-        
     }
-    
-    // Facebook Delegate Methods
-    
-    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
-        println("User Logged In")
-        performSegueWithIdentifier("login_segue", sender: nil);
-        
-        //perform a segue
-    }
-    
-    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
-        println("User: \(user)")
-        println("User ID: \(user.objectID)")
-        println("User Name: \(user.name)")
-        var userEmail = user.objectForKey("email") as String
-        println("User Email: \(userEmail)")
-    }
-    
-    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
-        println("User Logged Out")
-    }
-    
-    func loginView(loginView : FBLoginView!, handleError:NSError) {
-        println("Error: \(handleError.localizedDescription)")
-    }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-        if segue.identifier == "login_segue" {
-            println("performing segue")
-            let vc = segue.destinationViewController as ViewController2
-            
-        }
-        
-    }
-    
+
+
 }
+
