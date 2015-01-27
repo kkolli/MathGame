0c71010caae1846e677023035cbf5c2aeb87b5fe
Tyler Levine <tylevine@cisco.com>
2015-01-27 12:07:19 -0800

    spritekit is now working on the main game scene

---
+++ client/GameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GameViewController.swift
+//  Speedy
+//
+//  Created by Tyler Levine on 1/27/15.
+//  Copyright (c) 2015 Krishna Kolli. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class GameViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+        if let scene = GameScene(size: view.frame.size) as GameScene? {
+            // Configure the view.
+            let skView = self.view as SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            skView.showsPhysics = true
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = false
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning();
+        
+    }
+}
