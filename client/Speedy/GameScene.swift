da2cb85a07e9e236daa55ced43d762af36547a51
Krishna Kolli <kkolli@ucsd.edu>
2015-01-28 23:05:25 -0800

    Added Basic Grid System

--- client/Speedy/GameScene.swift
+++ client/Speedy/GameScene.swift
@@ -9,23 +9,20 @@
 import SpriteKit
 
 class GameScene : SKScene {
-    let canvasHeight: UInt32 = 800  //CHANGE MAGIC NUMBER
+    //TODO: Put constant numbers in CAPS
+    let canvasHeight: UInt32 = 800 //CHANGE MAGIC NUMBER
     let canvasWidth:UInt32 = 800   //CHANGE MAGIC NUMBER
+    let leftColumn:CGFloat = 50    //CHANGE MAGIC NUMBER
+    let middleColumn:CGFloat = 190   //CHANGE MAGIC NUMBER
+    let rightColumn:CGFloat = 325   //CHANGE MAGIC NUMBER
+    let startHeight:CGFloat = 600   //CHANGE MAGIC NUMBER
+    
+    let Size = CGSize(width:24, height:30)           /*Code for GridLayout*/
+    let GridSpacing = CGSize(width:120, height:20)
+    let RowCount = 8
+    let ColCount = 3
     
     var contentCreated = false
-    
-    enum InvaderType {
-        case A
-        case B
-        case C
-    }
-    
-    let kInvaderSize = CGSize(width:24, height:16)
-    let kInvaderGridSpacing = CGSize(width:12, height:12)
-    let kInvaderRowCount = 6
-    let kInvaderColCount = 6
-    
-    let kInvaderName = "invader"
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -33,99 +30,98 @@ class GameScene : SKScene {
         if (!contentCreated) {
             createContent()
             contentCreated = true
-            setupInvaders()
+            setupColumns()
         }
     }
     
     func createContent() {
-        
-        let invader = SKSpriteNode()
-        invader.position = CGPoint(x: size.width/2, y: size.height/2)
-        addChild(invader)
         self.backgroundColor = SKColor.lightGrayColor()
     }
     
     func drawSpeedy(){
         //Draw Column1
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Number!";
-        myLabel.fontSize = 35;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        myLabel.text = "Number";
+        myLabel.fontSize = 18;
+        myLabel.position = CGPoint(x:leftColumn, y:startHeight);
         self.addChild(myLabel)
         
+        //Draw Column2
+        let myLabel2 = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel2.text = "Operator";
+        myLabel2.fontSize = 18;
+        myLabel2.position = CGPoint(x:middleColumn, y:startHeight);
+        self.addChild(myLabel2)
         
+        //Draw Column3
+        let myLabel3 = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel3.text = "Number";
+        myLabel3.fontSize = 18;
+        myLabel3.position = CGPoint(x:rightColumn, y:startHeight);
+        self.addChild(myLabel3)
     }
     
-    func makeInvaderOfType(invaderType: InvaderType) -> (SKNode) {
+    func setupColumns() {
         
         // 1
-        var invaderColor: SKColor
-        
-        switch(invaderType) {
-        case .A:
-            invaderColor = SKColor.redColor()
-        case .B:
-            invaderColor = SKColor.greenColor()
-        case .C:
-            invaderColor = SKColor.blueColor()
-        default:
-            invaderColor = SKColor.blueColor()
-        }
-        
-        // 2
-        let invader = SKSpriteNode(color: invaderColor, size: kInvaderSize)
-        invader.name = kInvaderName
-        
-        return invader
-    }
-    
-    func setupInvaders() {
-        
-        // 1
-        let baseOrigin = CGPoint(x:size.width / 3, y:180)
-        for var row = 1; row <= kInvaderRowCount; row++ {
-            
-            var invaderType: InvaderType
-            if row % 3 == 0 {
-                invaderType = .A
-            } else if row % 3 == 1 {
-                invaderType = .B
-            } else {
-                invaderType = .C
-            }
+        let baseOrigin = CGPoint(x:leftColumn, y:startHeight - 530)  //Starting position to create Grid
+        for var row = 1; row <= RowCount; row++ {
+
             
             // 3
-            let invaderPositionY = CGFloat(row) * (kInvaderSize.height * 2) + baseOrigin.y
-            var invaderPosition = CGPoint(x:baseOrigin.x, y:invaderPositionY)
+            let PositionY = CGFloat(row) * (Size.height * 2) + baseOrigin.y
+            var Position = CGPoint(x:baseOrigin.x, y:PositionY)
             
             // 4
-            for var col = 1; col <= kInvaderColCount; col++ {
+            for var col = 1; col <= ColCount; col++ {
                 
-                var invader = makeInvaderOfType(invaderType)
-                invader.position = invaderPosition
-                addChild(invader)
+                /*var invader = SKSpriteNode(color:SKColor.redColor(), size:Size)
+                invader.position = Position
+                addChild(invader)*/
                 
-                invaderPosition = CGPoint(x: invaderPosition.x + kInvaderSize.width + kInvaderGridSpacing.width, y: invaderPositionY)
+                let shape = SKShapeNode(circleOfRadius: 20)
+                shape.strokeColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.5)
+                shape.lineWidth = 4
+                let text = SKLabelNode(text: String(3))
+                // we set the font
+                text.fontSize = 16.0
+                // we nest the text label in our circle
+                shape.addChild(text)
+                
+                // we set initial random positions
+                shape.position = Position
+                // we add each circle to the display list
+                self.addChild(shape)
+                
+                
+                
+                Position = CGPoint(x: Position.x + Size.width + GridSpacing.width, y: PositionY)
             }
         }
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* touch has begun */
-        
+    
+    func createCircle(){
         let shape = SKShapeNode(circleOfRadius: 20)
         shape.strokeColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.5)
         shape.lineWidth = 4
         let text = SKLabelNode(text: String(3))
+        text.color = UIColor.whiteColor()
         // we set the font
         text.fontSize = 6.0
-        // we nest the text label in our ball
+        // we nest the text label in our circle
         shape.addChild(text)
         
         // we set initial random positions
         shape.position = CGPoint (x: CGFloat(arc4random()%(canvasWidth)), y: CGFloat(arc4random()%(canvasHeight)))
         // we add each circle to the display list
         self.addChild(shape)
+
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* touch has begun */
+        
     }
     
     override func update(currentTime: CFTimeInterval) {
