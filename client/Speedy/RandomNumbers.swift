c2c92bffc72b28a257d97ccf11cb802870417808
Edward Chiou <edchiou@ucsd.edu>
2015-02-03 11:31:15 -0800

    Edited Info.plist and added comments

--- client/Speedy/RandomNumbers.swift
+++ client/Speedy/RandomNumbers.swift
@@ -28,6 +28,7 @@ class RandomNumbers {
         weightedList = []
     }
     
+    //Use this constructor
     init(difficulty: Int){
         self.difficulty = difficulty
         self.target = 0
@@ -39,15 +40,10 @@ class RandomNumbers {
         self.weightedList = generateWeightedList(weights)
     }
     
-    func generateGeometricDist() -> Int{
-        let p = 0.25
-        var random = Double(arc4random() / UInt32.max)
-        var denom = log(1.0 - p)
-        var exponentialDist = log(random) / denom
-        
-        return Int(floor(exponentialDist))
-    }
-    
+    /*
+     * We want 1-10 to appear more than 11-100, so 1-10 is assigned a 
+     * higher probability.
+     */
     func generateWeights() -> [Double]{
         let primaryProbability = 0.055
         let secondaryProbability = 0.005
@@ -68,6 +64,11 @@ class RandomNumbers {
         return weights
     }
     
+    /*
+     * Generates weighted number list to provide the RNG that we want.
+     * List contains 1000 numbers. If probability of a number is x, then
+     * it will appear x * 1000 times.
+     */
     func generateWeightedList(weights: [Double]) -> [Int]{
         var weightedList: [Int] = []
         
@@ -82,6 +83,9 @@ class RandomNumbers {
         return weightedList
     }
     
+    /*
+     * Call this to generate a random number.
+     */
     func generateNumber() -> Int{
         var randomNumber = Int(arc4random_uniform(UInt32(1000)))
         
