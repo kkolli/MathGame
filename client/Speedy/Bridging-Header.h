b730867dc5e2ec03f04d5a9e3df724b09356c3f1
Tyler Levine <tylevine@cisco.com>
2015-02-26 20:48:52 -0800

    readd bridging header (where did this go?) and remove ViewController2.swift

---
+++ client/Speedy/Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Bridging-Header.h
+//  Speedy
+//
+//  Created by John Chou on 1/23/15.
+//  Copyright (c) 2015 Krishna Kolli. All rights reserved.
+//
+
+#ifndef Speedy_Bridging_Header_h
+#define Speedy_Bridging_Header_h
+
+//
+// Bridging-Header.h
+// SwiftBook
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
