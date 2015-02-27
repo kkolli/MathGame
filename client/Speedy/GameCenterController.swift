fdc2b80c5faea68c6b87c612a800cce250cda10a
John Chou <choucalate@Johnnays-MBP.local>
2015-02-26 20:49:01 -0800

    connecting with mpc framework

--- client/Speedy/GameCenterController.swift
+++ client/Speedy/GameCenterController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class GameCenterController: UIViewController {
+class GameCenterController: UIViewController, MCBrowserViewControllerDelegate {
     /// GameCenter
-    var gameCenter: GameCenter!
+//    var gameCenter: GameCenter!
+    var currentPlayer:String!
+    
+    var appDelegate:AppDelegate!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.gameCenter = GameCenter(rootViewController: self)
-        self.gameCenter.loginToGameCenter()
+        println("IN GAME CENTER CONTROLLER")
+        appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        appDelegate.mpcHandler.setupPeerWithDisplayName(UIDevice.currentDevice().name)
+        appDelegate.mpcHandler.setupSession()
+        appDelegate.mpcHandler.advertiseSelf(true)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "peerChangedStateWithNotification:", name: "MPC_DidChangeStateNotification", object: nil)
+        
+        //        self.gameCenter = GameCenter(rootViewController: self)
+        //        self.gameCenter.loginToGameCenter()
+    }
+    @IBAction func connectWithPlayer(sender: AnyObject) {
+        if appDelegate.mpcHandler.session != nil{
+            appDelegate.mpcHandler.setupBrowser()
+            appDelegate.mpcHandler.browser.delegate = self
+            
+            self.presentViewController(appDelegate.mpcHandler.browser, animated: true, completion: nil)
+            
+        }
+    }
+    
+    func peerChangedStateWithNotification(notification:NSNotification){
+        println("PEER CHANGED STATE?!?!")
+        let userInfo = NSDictionary(dictionary: notification.userInfo!)
+        
+        let state = userInfo.objectForKey("state") as Int
+        
+        if state != MCSessionState.Connecting.rawValue{
+            self.navigationItem.title = "Connected"
+        }
+        
+    }
+    
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController!) {
+        appDelegate.mpcHandler.browser.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController!) {
+        appDelegate.mpcHandler.browser.dismissViewControllerAnimated(true, completion: nil)
     }
 }
