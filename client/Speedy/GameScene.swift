238d7e4b3761ff722d9346596bdc90b40c5093a4
Tyler Levine <tylevine@cisco.com>
2015-02-26 21:08:02 -0800

    fix merge defects, app is building and needs verification

--- client/Speedy/GameScene.swift
+++ client/Speedy/GameScene.swift
@@ -130,118 +130,6 @@ class GameScene : SKScene, SKPhysicsContactDelegate {
         println("Contact 2")
     }
     
-    let Size = CGSize(width:24, height:30)           /*Code for GridLayout*/
-    let GridSpacing = CGSize(width:120, height:20)
-    let RowCount = 8
-    let ColCount = 3
-    
-    //let Node:UInt32 = 0x1 << 0
-    //let NonNode:UInt32 = 0x1 << 1
-    
-    */
-    let randomNumbers = RandomNumbers(difficulty: 5) //Hardcoded difficulty value
-    let randomOperators = RandomOperators(difficulty: 5) //Hardcoded difficulty value
-    
-    //var contentCreated = false
-    //var wayPoints: [CGPoint] = []
-    var activeNode: SKNode?
-    
-    override func didMoveToView(view: SKView) {
-        let boardController = BoardController(scene: self, debug: true)
-        
-        setUpPhysics()
-        /* Setup your scene here
-        //drawSpeedy()
-        if (!contentCreated) {
-            createContent()
-            contentCreated = true
-            setupColumns()
-        }*/
-    }
-    
-    func setUpPhysics(){
-        self.physicsWorld.gravity = CGVectorMake(0, 0)
-        // we put contraints on the top, left, right, bottom so that our balls can bounce off them
-        let physicsBody = SKPhysicsBody (edgeLoopFromRect: self.frame)
-        physicsBody.dynamic = false
-        physicsBody.categoryBitMask = 0xFFFFFFFF
-        self.physicsBody = physicsBody
-        self.physicsBody?.restitution = 0.1
-        self.physicsBody?.friction = 0.0
-        self.physicsWorld.contactDelegate = self;
-    }
-   /*
-    func drawSpeedy(){
-        //Draw Column1
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Number";
-        myLabel.fontSize = 18;
-        myLabel.position = CGPoint(x:leftColumn, y:startHeight);
-        self.addChild(myLabel)
-        
-        //Draw Column2
-        let myLabel2 = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel2.text = "Operator";
-        myLabel2.fontSize = 18;
-        myLabel2.position = CGPoint(x:middleColumn, y:startHeight);
-        self.addChild(myLabel2)
-        
-        //Draw Column3
-        let myLabel3 = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel3.text = "Number";
-        myLabel3.fontSize = 18;
-        myLabel3.position = CGPoint(x:rightColumn, y:startHeight);
-        self.addChild(myLabel3)
-    }
-    
-    func setupColumns() {
-        
-        // 1
-        let baseOrigin = CGPoint(x:leftColumn, y:startHeight - 530)  //Starting position to create Grid
-        for var row = 1; row <= RowCount; row++ {
-
-            // 3
-            let PositionY = CGFloat(row) * (Size.height * 2) + baseOrigin.y
-            var Position = CGPoint(x:baseOrigin.x, y:PositionY)
-            
-            // 4
-            for var col = 1; col <= ColCount; col++ {
-                if col % 2 == 0{
-                    let op = OperatorCircle(col: col, operatorSymbol: randomOperators.generateOperator())
-                    op.setPosition(Position)
-                    
-                    self.addChild(op.parentNode!)
-                }else{
-                    let number = NumberCircle(num: randomNumbers.generateNumber(), col: col)
-                    number.setPosition(Position)
-                    
-                    self.addChild(number.parentNode!)
-                }
-                
-                Position = CGPoint(x: Position.x + Size.width + GridSpacing.width, y: PositionY)
-            }
-        }
-    }
-    */
-    
-    func didBeginContact(contact: SKPhysicsContact) {
-        println("CONTACT")
-        
-        var firstBody: SKPhysicsBody
-        var secondBody: SKPhysicsBody
-        
-        if contact.bodyA.joints.count <= 1 && contact.bodyB.joints.count <= 1 {
-            var myJoint = SKPhysicsJointPin.jointWithBodyA(contact.bodyA, bodyB: contact.bodyB,
-                anchor: contact.bodyA.node!.position)
-            myJoint.frictionTorque = 1.0
-            self.physicsWorld.addJoint(myJoint)
-        }
-        
-    }
-    
-    func didEndContact(contact: SKPhysicsContact) {
-        println("Contact 2")
-    }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* touch has begun */
@@ -299,16 +187,6 @@ class GameScene : SKScene, SKPhysicsContactDelegate {
         
         touchedNode.position.x = touchLocation.x
         touchedNode.position.y = touchLocation.y
-        
-        //addPoint(touchLocation)
-    }
-
-
-    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
-       // wayPoints.removeAll(keepCapacity: false)
-        if let physBody = activeNode?.physicsBody {
-            physBody.dynamic = true
-        }
         
         //addPoint(touchLocation)
     }
