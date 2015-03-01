1d4f7b208432eab06e44513ee824265f30e450ff
John Chou <choucalate@Johnnays-MBP.local>
2015-02-28 23:05:46 -0800

    adding some changes to configure and resolve the client and server and also go to the game

--- client/Speedy/MultiplayerGameViewController.swift
+++ client/Speedy/MultiplayerGameViewController.swift
@@ -6,13 +6,29 @@
 //  Copyright (c) 2015 Krishna Kolli. All rights reserved.
 //
 
+import SpriteKit
 import UIKit
 
-class MultiplayerViewController: UIViewController {
+class MultiplayerGameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        println("in multiplayer view controller")
+        if let scene = GameScene(size: view.frame.size) as GameScene? {
+            // Configure the view.
+            let skView = self.view as SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            //skView.showsPhysics = true
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = false
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
         // Do any additional setup after loading the view.
     }
 
--- client/Speedy/MultiplayerSetupViewController.swift
+++ client/Speedy/MultiplayerSetupViewController.swift
@@ -1,5 +1,5 @@
 //
-//  GameCenterController.swift
+//  MultiplayerSetupViewController.swift
 //  Speedy
 //
 //  Created by Krishna Kolli on 2/12/15.
@@ -9,12 +9,13 @@
 import UIKit
 import MultipeerConnectivity
 
-class GameCenterController: UIViewController, MCBrowserViewControllerDelegate {
-    /// GameCenter
-//    var gameCenter: GameCenter!
+class MultiplayerSetupViewController: UIViewController, MCBrowserViewControllerDelegate {
+
     var currentPlayer:String!
     
     var appDelegate:AppDelegate!
+    var id = UIDevice.currentDevice().identifierForVendor.UUIDString.utf8
+    var isServer:Bool!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,8 @@ class GameCenterController: UIViewController, MCBrowserViewControllerDelegate {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "peerChangedStateWithNotification:", name: "MPC_DidChangeStateNotification", object: nil)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleReceivedDataWithNotification:", name: "MPC_DidReceiveDataNotification", object: nil)
+        
+        
     }
     @IBAction func connectWithPlayer(sender: AnyObject) {
         if appDelegate.mpcHandler.session != nil{
@@ -43,26 +46,81 @@ class GameCenterController: UIViewController, MCBrowserViewControllerDelegate {
         let userInfo = NSDictionary(dictionary: notification.userInfo!)
         
         let state = userInfo.objectForKey("state") as Int
-        
-        if state != MCSessionState.Connecting.rawValue{
+        let otherUserPID = userInfo.objectForKey("peerID") as MCPeerID
+        if state != MCSessionState.Connecting.rawValue {
             self.navigationItem.title = "Connected"
-            performSegueToMultiplayer()
+            determineServer(otherUserPID)
         }
         
     }
     
+    func determineServer(peerID: MCPeerID) {
+        // first figure out who's the server
+        // If I'm the server, send data saying { "server": true } 
+        self.isServer = amIServer(peerID)
+        
+        // should probably make init some kind of constant
+        var msg = ["type" : "init", "server": self.isServer]
+        let messageData = NSJSONSerialization.dataWithJSONObject(msg, options: NSJSONWritingOptions.PrettyPrinted, error: nil)
+        var error:NSError?
+        
+        appDelegate.mpcHandler.session.sendData(messageData, toPeers: appDelegate.mpcHandler.session.connectedPeers, withMode: MCSessionSendDataMode.Reliable, error: &error)
+        
+        if error != nil{
+            println("error: \(error?.localizedDescription)")
+        }
+    }
+    
+    /*
+    Convert both to utf8 and then compute them
+    return true if I'm the server.  else false
+    */
+    func amIServer(peerID: MCPeerID) -> Bool {
+        let myIDArr = Array(appDelegate.mpcHandler.peerID.displayName.generate())
+        let otherIDArr = Array(peerID.displayName.generate())
+        
+        let iter_count = max(myIDArr.count, otherIDArr.count)
+        for i in 0...iter_count-1 {
+            if (myIDArr[i] > otherIDArr[i]) {
+                return true
+            } else {
+                return false
+            }
+        }
+        return false
+        
+        
+    }
+    
     func handleReceivedDataWithNotification(notification:NSNotification){
+        println("received some data with notification!")
         let userInfo = notification.userInfo! as Dictionary
         let receivedData:NSData = userInfo["data"] as NSData
         
         let message = NSJSONSerialization.JSONObjectWithData(receivedData, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSDictionary
         let senderPeerId:MCPeerID = userInfo["peerID"] as MCPeerID
         let senderDisplayName = senderPeerId.displayName
-        
-        if message.objectForKey("string")?.isEqualToString("New Game") == true{
-            
+        let msg_type: AnyObject? = message.objectForKey("type")
+        if msg_type?.isEqualToString("init") == true{
+            let isPeerServer = message.objectForKey("server")? as Bool
+            // here we have to make sure that if we're the server, then they're not, and if we're not the server, that they are
+            if isPeerServer != self.isServer {
+                // success!
+                println("resolved correctly, going to segue to multiplayer!!")
+                if appDelegate.mpcHandler.browser != nil {
+                  appDelegate.mpcHandler.browser.dismissViewControllerAnimated(true, completion: nil)
+                }
+                performSegueToMultiplayer()
+            } else {
+                println("there was an error for client server resolution- client: \(self.isServer) serveR: \(isPeerServer)")
+            }
         }
         
+        
+        
+    }
+    
+    func sendInitializationData() {
         
     }
     
@@ -73,7 +131,7 @@ class GameCenterController: UIViewController, MCBrowserViewControllerDelegate {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if segue.identifier == "multiplayer_segue" {
             println("performing segue to multiplayer")
-            let vc = segue.destinationViewController as MultiplayerViewController
+            let vc = segue.destinationViewController as MultiplayerGameViewController
         }
         
     }
