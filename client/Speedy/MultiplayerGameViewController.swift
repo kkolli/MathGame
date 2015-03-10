1b89930bfd890e74749da606b0275caa3acce7a2
Tyler Levine <tylevine@cisco.com>
2015-03-10 00:08:20 -0700

    refactor some code, implement first version of board header

--- client/Speedy/MultiplayerGameViewController.swift
+++ client/Speedy/MultiplayerGameViewController.swift
@@ -42,9 +42,9 @@ class MultiplayerGameViewController: UIViewController, SKPhysicsContactDelegate 
         println("in multiplayer view controller")
 
         scene = GameScene(size: view.frame.size)
-        boardController = BoardController(scene: scene!)
-        scene!.boardController = boardController
         numTargetNumbersMatched = 0
+        boardController = BoardController(scene: scene!, mode: .MULTI)
+   //     scene!.boardController = boardController
         updateTargetNumber()
         operatorsUsed = []
         finishedInit = false
@@ -230,7 +230,7 @@ class MultiplayerGameViewController: UIViewController, SKPhysicsContactDelegate 
     
     func updateTargetNumber(){
         if targetNumber != nil{
-            let numberCircleList = boardController!.circleList.filter{$0 is NumberCircle}
+            let numberCircleList = boardController!.gameCircles.filter{$0 is NumberCircle}
             let numberList = numberCircleList.map{($0 as NumberCircle).number!}
             targetNumber = boardController!.randomNumbers.generateTarget(numberList)
         }else{
