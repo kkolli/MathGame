6bfd6cd4047740dd912be6174641e2d822db6014
Tyler Levine <tylevine@cisco.com>
2015-03-11 13:19:46 -0700

    remove fps and node counters from multiplayer

--- client/Speedy/MultiplayerGameViewController.swift
+++ client/Speedy/MultiplayerGameViewController.swift
@@ -49,8 +49,8 @@ class MultiplayerGameViewController: UIViewController, SKPhysicsContactDelegate 
         
         // Configure the view.
         let skView = self.view as SKView
-        skView.showsFPS = true
-        skView.showsNodeCount = true
+        //skView.showsFPS = true
+        //skView.showsNodeCount = true
         //skView.showsPhysics = true
         /* Sprite Kit applies additional optimizations to improve rendering performance */
         skView.ignoresSiblingOrder = false
